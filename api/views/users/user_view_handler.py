dbdd553e694ab9b248a9229b945435377f349bfe
VictorCyprien <victor.cyprien@limayrac.fr>
2025-02-08 10:58:51 +0100

    Update views

--- api/views/users/user_view_handler.py
+++ api/views/users/user_view_handler.py
@@ -1,16 +1,20 @@
 from flask.views import MethodView
+from sqlalchemy.orm import Session
+
 from api.models.user import User
 from helpers.errors_file import BadRequest, ErrorHandler
 
 class UserViewHandler(MethodView):
-    def check_user_exists(self, input_data: dict):
-        if User.objects(email=input_data["email"]):
+    def check_user_exists(self, input_data: dict, session: Session):
+
+        # Check if email is already used
+        if session.query(User).filter(User.email == input_data["email"]).first():
             raise BadRequest(ErrorHandler.USER_EMAIL_ALREADY_USED)
         
         # Check if discord username is already used
-        if User.objects(discord_username=input_data["discord_username"]):
+        if session.query(User).filter(User.discord_username == input_data["discord_username"]).first():
             raise BadRequest(ErrorHandler.USER_DISCORD_USERNAME_ALREADY_USED)
         
         # Check if wallet address is already used
-        if User.objects(wallet_address=input_data["wallet_address"]):
+        if session.query(User).filter(User.wallet_address == input_data["wallet_address"]).first():
             raise BadRequest(ErrorHandler.USER_WALLET_ADDRESS_ALREADY_USED)
