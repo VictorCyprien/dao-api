0161ed733a25aaf5ad398185501fa67923850657
VictorCyprien <victor.cyprien@limayrac.fr>
2025-04-01 20:48:37 +0200

    Update wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,4 @@
-import subprocess
 from api.main import app
-from api.config import config
 
 from helpers.logging_file import Logger
 
@@ -9,14 +7,5 @@ logger = Logger()
 
 
 if __name__ == "__main__":
-    # Create Redis worker scheduler with subprocess
-    worker_process = subprocess.Popen([
-        'rq', 'worker',
-        '--name', config.SERVICE_NAME,
-        '--with-scheduler',
-    ])
-
-    logger.info(f"Redis worker scheduler started with process ID {worker_process.pid}")
-
     # Run the app
     app.run()
